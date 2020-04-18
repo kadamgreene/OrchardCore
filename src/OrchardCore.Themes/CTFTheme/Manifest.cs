2a5a9599fc74716af68c5d161d7941fee60140bb
Adam Greene <AGreene@cvent.com>
2020-04-18 11:23:07 -0300

    Started CTFTheme

---
+++ src/OrchardCore.Themes/CTFTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Catch the Fire Theme",
+    Author = "Catch the Fire Moncton",
+    Website = "https://ctfmoncton.com",
+    Version = "0.0.1",
+    Description = "A theme conforming to Catch the Fire branding guidelines"
+)]
